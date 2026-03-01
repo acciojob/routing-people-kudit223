80598cccda698f27582a891bca87196f24fd0b40
Udit Kumar <kudit223@gmail.com>
2026-03-01 18:43:30 +0530

    update

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 
-function UserDetails({data}){
-    const {id}=useParams();
-    const user=data.filter(usr=>usr.id===id);
-    return(<div>
-        <h1>User Details</h1>
-        <p>Name: {user.name}</p>
-        <p>Username: {user.username}</p>
-        <p>Email: {user.email}</p>
-        <p>Phone: {user.phone}</p>
-        <p>Website: {user.website}</p>
-    </div>)
+function UserDetails({ data }) {
+    const { id } = useParams();
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        setUser(() => data.filter(usr => usr.id === id));
+    },[])
+
+    return (<>
+        {user?<div>
+            <h1>User Details</h1>
+            <p>Name: {user.name}</p>
+            <p>Username: {user.username}</p>
+            <p>Email: {user.email}</p>
+            <p>Phone: {user.phone}</p>
+            <p>Website: {user.website}</p>
+        </div>:<div>Loading...</div>}
+    </>)
 }
 
 export default UserDetails;
